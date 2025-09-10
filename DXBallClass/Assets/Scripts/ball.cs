d5dffda8a3487e255ddc017327522e8ff8c090f9
SirazumMuniraTisha <tisha.ruet09@gmail.com>
2025-09-10 12:05:06 -0400

    bricks and wall added

--- DXBallClass/Assets/Scripts/ball.cs
+++ DXBallClass/Assets/Scripts/ball.cs
@@ -3,26 +3,33 @@ using UnityEngine;
 public class ball : MonoBehaviour
 {
     // Start is called before the first frame update
-     Rigidbody2D rb;
-     public float speed;
-     public Vector2 direction;
-    
+    Rigidbody2D rb;
+    public float speed;
+    public Vector2 direction;
+
     void Start()
     {
-        rb= GetComponent<Rigidbody2D>();
-        direction= Vector2.one.normalized; //(1,1)
-        
+        rb = GetComponent<Rigidbody2D>();
+        direction = Vector2.one.normalized; //(1,1)
+
     }
 
     // Update is called once per frame
     void Update()
-    {   
-        rb.linearVelocity= direction*speed;
+    {
+        rb.linearVelocity = direction * speed;
     }
 
-    void OnTriggerEnter2D(Collider2D collison){
+    void OnTriggerEnter2D(Collider2D collison)
+    {
         if (collison.gameObject.CompareTag("paddle"))
             direction.y = -direction.y;
-    
+        else if (collison.gameObject.CompareTag("brick"))
+        {
+            direction.y = -direction.y;
+            Destroy(collison.gameObject);
         }
+        else if (collison.gameObject.CompareTag("sideWall"))
+            direction.x = -direction.x;
+    }
 }
